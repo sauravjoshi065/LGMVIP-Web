1133833b2636ae5c85e2cfad47403aaa63298c08
sauravjoshi065 <95427366+sauravjoshi065@users.noreply.github.com>
2022-01-19 13:36:01 +0530

    Add files via upload

---
+++ Task 2/src/Extra Assests/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div class="contain">
+          <div class="col">
+            <h1>Let's Grow More Task 2 </h1>
+            <ul>
+              <li>
+                Mail: <a >saurabhjoshi065@gmail.com</a>
+              </li>
+              <li>
+                Call: <a>>+91 750 750 7341</a>
+              </li>
+            </ul>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Footer;
